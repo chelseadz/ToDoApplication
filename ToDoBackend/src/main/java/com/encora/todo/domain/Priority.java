93ce5d273aa80181c19698b1debe13379962dcb1
chelseadz <chelseadurazoxp@gmail.com>
2025-10-17 01:27:58 -0700

    old app new branch

--- ToDoBackend/src/main/java/com/encora/todo/domain/Priority.java
+++
@@ -1,6 +0,0 @@
-package com.encora.todo.domain;
-
-
-public enum Priority {
-    LOW, MEDIUM, HIGH, CRITICAL
-}
--- ToDoBackend/src/main/java/com/encora/todo/domain/SortField.java
+++
@@ -1,5 +0,0 @@
-package com.encora.todo.domain;
-
-public enum SortField{
-    CREATION_DATE, DUE_DATE, PRIORITY, DONE
-}
